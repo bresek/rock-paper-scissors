ea2271dd11d7b22440b7567eb096a95487ca114c
BENJAMIN RESEK <bresek@clarku.edu>
2021-03-24 20:20:27 -0400

    defined function for computers turn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+
+
+// check if script is conncted
 console.log("Script connected!")
+
+//define moves for game
+let moves = ["rock","paper","scissors"]
+
+function randomNumber(){
+  return Math.floor(Math.random()*moves.length)
+}
+
+
+
+function computerPlay(){
+  randMove = randomNumber()
+  move = moves[randMove]
+  console.log(move)
+}
