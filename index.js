3712a401684827f8944b18b1ad6e58890c3d0ec6
BENJAMIN RESEK <bresek@clarku.edu>
2021-03-25 10:53:49 -0400

    finished game function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function randomNumber() {
 function computerPlay() {
   randMove = randomNumber()
   move = moves[randMove]
-  console.log(move)
+  return move
 }
 
 //rock beats scissors
@@ -27,27 +27,50 @@ function playRound(playerSelection, computerSelection) {
 
   //logic to decide who wins
   if (playerSelection === computerSelection) {
-    return "Tie!"
+    return "tie"
   } else{
     if (playerSelection === "rock"){//handle rock
       if (computerSelection === "paper"){
-        return "You lose! paper beats rock!"
+        return "lose"
       }else{
-        return "You win! rock beats scissors!"
+        return "win"
       }
     }else if(playerSelection === "paper"){
       if (computerSelection ==="rock"){
-        return "You win! paper beats rock!"
+        return "win"
       }else{
-        return "You lose! scissors beats paper!"
+        return "lose"
       }
     }else{
       if(computerSelection === "paper"){
-        return "You win! scissors beats paper!"
+        return "win"
       }else{
-        return "You lose! rock beats scissors!"
+        return "lose"
       }
     }
   }
 
+}
+
+function game(rounds){
+  playerScore = 0;
+  computerScore = 0;
+  for (let i=0; i < rounds; i++){
+    playerMove = prompt("rock, paper, or scissors?")
+    computerMove = computerPlay()
+    result = playRound(playerMove, computerMove)
+    console.log(result + "! computer's move was " + computerMove)
+    if (result === "win"){
+      playerScore = playerScore + 1
+    }else if(result ="lose"){
+      computerScore = computerScore + 1
+    }
+  }
+  if (playerScore > computerScore){
+    return "You win the game! Score was:" + playerScore + "to" + computerScore
+  }else if (playerScore < computerScore) {
+    return "You lose the game! Score was:" + playerScore + "to" + computerScore
+  }else{
+    return "tie! Score was:" + playerScore + "to" + computerScore
+  }
 }
